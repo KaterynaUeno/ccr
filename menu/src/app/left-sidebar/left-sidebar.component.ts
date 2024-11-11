0c5313db701d4952ed80e7f161d348ed28aab0aa
Kateryna Ueno <katerynaueno@gmail.com>
2024-11-11 17:28:12 +0000

    set up

---
+++ menu/src/app/left-sidebar/left-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-left-sidebar',
+  templateUrl:'./left-sidebar.component.html',
+  styleUrls:['./left-sidebar.component.css'],
+  standalone: true,
+  imports: []
+})
+
+export class LeftSideBarComponent {}
