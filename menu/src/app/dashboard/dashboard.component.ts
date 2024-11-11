5037e7e2279013cbd74f997547797172f216389c
Kateryna Ueno <katerynaueno@gmail.com>
2024-11-11 17:07:49 +0000

    set up

---
+++ menu/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl:'./dashboard.component.html',
+  styleUrls:['./dashboard.component.css'],
+  standalone: true,
+  imports: []
+})
+
+export class DashboardComponent {}
---
+++ menu/src/app/pages/pages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-pages',
+  templateUrl:'./pages.component.html',
+  styleUrls:['./pages.component.css'],
+  standalone: true,
+  imports: []
+})
+
+export class PagesComponent {}
